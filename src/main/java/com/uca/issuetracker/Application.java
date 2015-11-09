9eb5b66341f6d8282120dfaa411b91c985e8fe2d
Michael Hedges <michael.hedges@few.io>
2015-11-08 18:43:27 -0600

    first commit

---
+++ src/main/java/com/uca/issuetracker/Application.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uca.issuetracker;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import services.Service;
+
+/**
+ *
+ * @author danger
+ */
+public class Application {    
+    
+    public static void main(String [] args)
+    {
+        Service service = new Service();
+        try {
+            System.out.println(service.list("Project"));
+        } catch (Exception ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/main/java/services/Service.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+/**
+ *
+ * @author danger
+ */
+public class Service {
+    private final String baseURl = "https://api.parse.com/1";
+    private final String parseAppId = "ejdm9iw3ff8CG2JVWX33CiLkpzwngMYTMyQ4CNE0";
+    private final String restApiKey = "ZokE5oRI2ibLXmaPcsxLoXx4eCvnYHHZcc4cAnuu";
+    private ClientRequest request;
+    private ClientResponse response;
+    private JSONParser parser;
+    
+
+    
+    /**
+     * Attach Given Headers
+     */
+    private void prepareRequest() {
+        this.request.header("X-Parse-Application-Id", this.parseAppId);
+        this.request.header("X-Parse-REST-API-Key", this.restApiKey);
+    }
+    
+    /**
+     * List a given class
+     * @param className
+     * @return 
+     * @throws java.lang.Exception 
+     */
+    public String list(String className) throws Exception{
+        String url = this.baseURl + "/classes/" + className;
+        System.out.println("list: " + url);
+        this.request = new ClientRequest(url);
+        this.prepareRequest();
+        this.response = request.get(String.class);
+        return (String) this.response.getEntity();
+    }
+}
